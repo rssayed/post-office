7e75061fe63b12e3d2f1aa2fd3b80370f8314b92
rssayed <43046453+rssayed@users.noreply.github.com>
2021-04-06 16:58:35 -0500

    remove rsuite dependency

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Header from './components/header';
 import Login from './components/login';
 
 import Deletepackage from './components/Deletepackage';
