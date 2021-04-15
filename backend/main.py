1bbba6c7d5de5e480c9e99d95b1ef67e487e35b9
rigby0105 <rogerramirez00@gmail.com>
2021-04-15 17:20:36 -0500

    Fixed orderhistory to reflect changes in what data is being returned Wrote create_package function with as much data as we are adding Wrote update_package function

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ from flask_mysql_connector import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.secret_key = 'super secret key' # lets cookies work so flash() can work
+app.secret_key = 'super secret key'  # lets cookies work so flash() can work
 
 # enter username and password
 app.config['MYSQL_USER'] = 'root'
@@ -64,30 +64,7 @@ def profile():
                                                                  customer_id))
         mysql.connection.commit()
 
-    return render_template('/profile')  # Documentation shows rendering a .html file, not sure if this is correct
-
-    # leaving this original code I did commented out in case it is helpful for the login page
-
-    #     if profile.validate():
-    #
-    #         fname = request.form.get('fname')
-    #         lname = request.form.get('lname')
-    #         street_address = request.form.get('street_address')
-    #         city = request.form.get('city')
-    #         state = request.form.get('state')
-    #         zipcode = request.form.get('zipcode')
-    #         email = request.form.get('email')
-    #         customer_password = request.form.get('customer_password')
-    #         existing_user = cur.execute('SELECT * FROM customer WHERE email = ?', (email,)).fetchone()
-    #         if existing_user is None:
-    #             cur.execute("INSERT INTO customer(fname, lname, street_address, city, state, zipcode, password, "
-    #                         "email) VALUES (%s, %s, %s, %s, %s, %s, %s, %s", (fname, lname, street_address, city,
-    #                                                                           state, zipcode, generate_password_hash(
-    #                                                                             customer_password), email))
-    #             return 'success'
-    #         flash('A user exists with that email address.')
-    #         return redirect(url_for('auth.login'))  # make sure this is correct. Want to try to redirect to the login page if user already exists
-    # return render_template('/login')  # not sure if this is correct either. Documentation usually renders a static html file
+    return render_template('/profile.js')  # Documentation shows rendering a .html file, not sure if this is correct
 
 
 @app.route('/orderHistory', methods=['GET', 'POST'])
@@ -98,11 +75,11 @@ def order_history():
         customer_id = request.form.get('customer_id')
 
         cur.execute(
-            """SELECT package.%s, deliver_to, receiver.street_address, receiver.city, receiver.zipcode, 
-            shipping_date, expected_delivery 
+            """SELECT package.tracking_id, deliver_to, shipping_date, expected_delivery 
             FROM package, receiver, orders, customer'
-            WHERE customer.%s = orders.%s AND orders.%s = package.%s""",
-            (tracking_id, customer_id, customer_id, tracking_id, tracking_id))
+            WHERE tracking_id=%s AND customer_id=%s AND
+            customer.customer_id = orders.customer_id AND orders.tracking_id = package.tracking_id""",
+            (tracking_id, customer_id))
         # This should return all the information we want to display based on the user input
     output = cur.fetchall()
     return str(output)
@@ -119,6 +96,50 @@ def tracking_history():
     return str(output)
 
 
+@app.route('/Update_Package', methods=['GET', 'POST'])
+def update_package():
+    cur = mysql.connection.cursor()
+    if request.method == 'POST':
+        tracking_id = request.form['tracking_id']
+        post_office_id = request.form['facility_id']
+        time_in = request.form['time_in']
+        time_out = request.form['time_out']
+        delivery_status = request.form['is_delivered']
+
+        cur.execute("""UPDATE delivers 
+                        SET time_in=%s, time_out=%s, is_delivered=%s
+                        WHERE package.tracking_id=%s AND post_office.facility_id=%s AND 
+                        orders.tracking_id=package.tracking_id AND orders.customer_id=customer.customer_id""",
+                    (time_in, time_out, delivery_status, tracking_id, post_office_id))
+        mysql.connection.commit()
+    return render_template('/Update_Package.js')
+
+
+@app.route('/CreatePackage', methods=['GET', 'POST'])
+def create_package():
+    cur = mysql.connection.cursor()
+    if request.method == 'POST':
+        shipping_date = request.form['shipping_date']
+        shipping_type = request.form['type']
+        weight = request.form['weight']
+        customer_id = request.form['customer_id']
+        name = request.form['name']
+        street_address = request.form['street_address']
+        city = request.form['city']
+        state = request.form['state']
+        zipcode = request.form['zipcode']
+
+# not sure if these will be valid as it doesn't fulfill all of the fields for creating each one of these
+        cur.execute("""INSERT INTO package(shipping_date, type, weight, deliver_to)
+                    VALUES (%s, %s, %s, %s)""", (shipping_date, shipping_type, weight, name))
+        cur.execute("""INSERT INTO receiver(name, street_address, city, state, zipcode)
+                    VALUES (%s, %s, %s, %s, %s)""", (name, street_address, city, state, zipcode))
+        cur.execute("""INSERT INTO orders(customer_id)
+                    VALUES (customer_id)""", customer_id)
+        mysql.connection.commit()
+    return render_template('/CreatePackage.js')
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     cur = mysql.connection.cursor()
@@ -127,23 +148,23 @@ def login():
         password = request.form['password']
         customerUsernames = cur.execute("SELECT mobile_number FROM customer")
         employeeUsernames = cur.execute("SELECT employee_email FROM employee")
-        for i in customerUsernames
-            if username == i
-                customerPW = cur.execute("SELECT customer_password FROM customer WHERE mobile_number=%s",(username))
-                if password == customerPW
+        for i in customerUsernames:
+            if username == i:
+                customerPW = cur.execute("SELECT customer_password FROM customer WHERE mobile_number=%s", (username))
+                if password == customerPW:
                     return redirect('/profile')
-                return('Incorrect password')
-        for i in employeeUsernames
-            if username == i
-                employeePW = cur.execute("SELECT employee_password FROM employee WHERE employee_email=%s",(username))
-                if password == employeePW
+                return ('Incorrect password')
+        for i in employeeUsernames:
+            if username == i:
+                employeePW = cur.execute("SELECT employee_password FROM employee WHERE employee_email=%s", (username))
+                if password == employeePW:
                     return redirect('/profile')
-                return('Incorrect password')
+                return ('Incorrect password')
         return ("login again")
 
     mysql.connection.commit()
     # cur.close() ?
-    return render_template('login.html') # example template for testing, change later
+    return render_template('login.html')  # example template for testing, change later
 
 
 @app.route('/delete', methods=['GET', 'POST'])
@@ -152,21 +173,21 @@ def delete():
     if request.method == 'POST':
         tracking_id = request.form['tracking_number']
         orders = cur.execute("SELECT tracking_id from orders")
-        for i in orders
-            if tracking_id == i
-                cur.execute("DELETE FROM orders WHERE tracking_id=%s",(tracking_id))
-                return('Sucessfully deleted package')
+        for i in orders:
+            if tracking_id == i:
+                cur.execute("DELETE FROM orders WHERE tracking_id=%s", (tracking_id))
+                return ('Sucessfully deleted package')
                 # flash('Sucessfully deleted package')
                 # return render_template('delete.html')
-            else
-                return('Package not found')
+            else:
+                return ('Package not found')
                 # flash('Package not found')
                 # return render_template('delete.html')
-        return('tracking id not found')
+        return ('tracking id not found')
 
     mysql.connection.commit()
     # cur.close() ?
-    return render_template('DeletePackage.js') # replace with actual one
+    return render_template('DeletePackage.js')  # replace with actual one
 
 
 # @app.route('/')
