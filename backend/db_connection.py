89cb3ae266f604516237dfab33f06ec9aa68ee5b
Days0n <davidsonvu@gmail.com>
2021-04-22 18:40:53 -0500

    tiny error fix maybe on db_connection.py

--- backend/db_connection.py
+++ backend/db_connection.py
@@ -2,12 +2,15 @@
 # -*- coding: utf-8 -*-
 import pymysql
 
-host = 'aws-snailmail.c2s7bdbtbg0f.us-east-2.rds.amazonaws.com'
-user = 'admin'
-password = 'Heartless1234'
-database = 'aws-snailmail'
+# host = 'aws-snailmail.c2s7bdbtbg0f.us-east-2.rds.amazonaws.com'
+# user = 'admin'
+# password = 'Heartless1234'
+# database = 'aws-snailmail'
 
-connection = pymysql.connect(host, user, password, database)
+connection = pymysql.connect(host="aws-snailmail.c2s7bdbtbg0f.us-east-2.rds.amazonaws.com",user="admin",password="Heartless1234",database="aws-snailmail")
+# connection = pymysql.connect(host, user, password, database)
+
+
 with connection:
     cur = connection.cursor()
     cur.execute("SELECT VERSION()")
