249d58ca6054f572d4e15e50beeecfaf1a234697
rssayed <43046453+rssayed@users.noreply.github.com>
2021-04-25 18:09:20 -0500

    added getUserId form w/o fetch

--- src/components/getUserId.js
+++ src/components/getUserId.js
@@ -1,8 +1,51 @@
 import React from 'react';
+import Textarea from 'react-rainbow-components/components/Textarea';
 
-export default function GetUserId() {
-
-    return(
-        null
-    );
+const fnameContainerStyles = {
+    margin: 'auto',
+    maxWidth: 300,
+    top: 150
 }
+
+const lnameContainerStyles = {
+    margin: 'auto',
+    maxWidth: 300,
+    top: 175
+}
+
+const emailContainerStyles = {
+    margin: 'auto',
+    maxWidth: 300,
+    top: 200
+}
+
+class GetUserId extends React.Component {
+
+    render(){
+        return(
+            <form name='getUserIdForm'>
+                <h1 align='center'>Get User ID</h1>
+                <Textarea
+                    label='First Name'
+                    name='fname'
+                    rows='1'
+                    style={fnameContainerStyles}
+                />
+                <Textarea
+                    label='Last Name'
+                    name='lname'
+                    rows='1'
+                    style={lnameContainerStyles}
+                />
+                <Textarea
+                    label='Email'
+                    name='email'
+                    rows='1'
+                    style={emailContainerStyles}
+                />
+            </form>
+        )
+    }
+}
+
+export default GetUserId;
