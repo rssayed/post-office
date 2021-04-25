57a8580a211aef21be3d5f64f4f1ac0948ce4a06
rssayed <43046453+rssayed@users.noreply.github.com>
2021-04-25 18:15:30 -0500

    updated getUserId

--- src/components/getUserId.js
+++ src/components/getUserId.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Textarea from 'react-rainbow-components/components/Textarea';
+import Button from 'react-rainbow-components/components/Button';
 
 const fnameContainerStyles = {
     margin: 'auto',
@@ -19,7 +20,46 @@ const emailContainerStyles = {
     top: 200
 }
 
+const buttonContainerStyle = {
+    backgroundColor: '#0645AE',
+    borderColor: '#0645AE',
+    color: '#ffffff',
+    margin: 'auto',
+    display:'block',
+    top: 250
+}
+
 class GetUserId extends React.Component {
+
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            fname: "",
+            lname:"",
+            email: ""
+        }
+        this.handleSubmit= this.handleSubmit.bind(this);
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const form = new FormData(document.getElementById('getUserIdForm'));
+        fetch('http://localhost:5000/backend/Tracking_history', {
+            method: 'POST',
+            body: form,
+            })
+            .then(response => response.json())
+            .then(result => {
+                console.log('Success:', result);            //need to figure out how to print the response on the webpage
+                this.setState({tracking_history: result});  // for some reason the backend is returning an array of arrays, get first array
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+
+            alert("Tracking Successful!");
+    }
 
     render(){
         return(
@@ -42,6 +82,15 @@ class GetUserId extends React.Component {
                     name='email'
                     rows='1'
                     style={emailContainerStyles}
+                />
+                <Button
+                    label='Get'
+                    name='getButton'
+                    size='medium'
+                    onClick={this.handleSubmit}
+                    style={buttonContainerStyle}
+                    variant='base'
+                    type="submit"
                 />
             </form>
         )
