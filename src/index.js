0eaf5c0ef23e075282e8d5f33060980b3e2b4592
TsmHorns <53026856+TsmHorns@users.noreply.github.com>
2021-04-26 22:42:21 -0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-<<<<<<< Updated upstream
-import './index.css';
-=======
->>>>>>> Stashed changes
 
 ReactDOM.render(
   <React.StrictMode>
